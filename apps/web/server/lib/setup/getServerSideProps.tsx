70ae4bd02a4fe77ac9cb0074e234dd31cd4b9eee
Sarthak2 <aniki123.ai@gmail.com>
2025-06-13 18:37:09 +0530

    test: trigger CI via fork

--- apps/web/server/lib/setup/getServerSideProps.tsx
+++ apps/web/server/lib/setup/getServerSideProps.tsx
@@ -36,7 +36,6 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
       },
     });
   }
-
   const isFreeLicense = (await getDeploymentKey(deploymentRepo)) === "";
 
   return {
